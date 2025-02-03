1dd85d9411bed412affa76d9f6c3bc51eecfc0aa
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-03 15:42:32 -0300

    feat: icon button component

--- web/src/components/attendee-list.tsx
+++ web/src/components/attendee-list.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
+import { IconButton } from './icon-button'
 
 export function AttendeeList() {
   return (
@@ -41,9 +42,9 @@ export function AttendeeList() {
               <td className='py-3 px-4 text-sm text-zinc-300'>2022-01-01</td>
               <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atrás</td>
               <td className='py-3 px-4 text-sm text-zinc-300'>
-                <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                <IconButton transparent className='bg-black/20 border border-white/10 rounded-md p-1.5'>
                   <MoreHorizontal className='size-4' />
-                </button>
+                </IconButton>
               </td>
             </tr>
           </tbody>
@@ -57,21 +58,21 @@ export function AttendeeList() {
                   <span>Página 1 de 23</span>
 
                   <div className='flex gap-1.5'>
-                    <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                    <IconButton className='bg-white/10 border border-white/10 rounded-md p-1.5'>
                       <ChevronsLeft className='size-4' />
-                    </button>
+                    </IconButton>
 
-                    <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                    <IconButton className='bg-white/10 border border-white/10 rounded-md p-1.5'>
                       <ChevronLeft className='size-4' />
-                    </button>
+                    </IconButton>
 
-                    <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                    <IconButton className='bg-white/10 border border-white/10 rounded-md p-1.5'>
                       <ChevronRight className='size-4' />
-                    </button>
+                    </IconButton>
 
-                    <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                    <IconButton className='bg-white/10 border border-white/10 rounded-md p-1.5'>
                       <ChevronsRight className='size-4' />
-                    </button>
+                    </IconButton>
                   </div>
                 </div>
               </td>
