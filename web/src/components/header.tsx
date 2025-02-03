49220ddbc86ec444aa1b37d3c226bef02354080e
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-03 15:42:18 -0300

    feat: nav link component

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,12 +1,13 @@
 import nlwUniteIcon from '../assets/logo.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
     <div className='flex items-center gap-5 py-2'>
       <img src={nlwUniteIcon} />
       <nav className='flex items-center gap-5'>
-        <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
-        <a href='' className='font-medium text-sm'>Participantes</a>
+        <NavLink href=''>Eventos</NavLink>
+        <NavLink href='' selected>Participantes</NavLink>
       </nav>
     </div>
   )
