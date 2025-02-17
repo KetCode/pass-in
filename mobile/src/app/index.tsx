a520c9ec47994a8177d957d0ccfc5a5cb103ee4a
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-17 11:20:22 -0300

    feat: page to enter with credencial

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,27 @@
-import {View, Text} from 'react-native'
+import {View, Image, StatusBar} from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Link } from 'expo-router'
+import { colors } from '@/styles/colors'
+import { Input } from '@/components/input'
+import { Button } from '@/components/button'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hey</Text>
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle='light-content' />
+      
+      <Image source={require('@/assets/logo.png')} className='h-16' resizeMode='contain' />
+
+      <View className='w-full mt-12 gap-3'>
+        <Input>
+          <MaterialCommunityIcons name='ticket-confirmation-outline' size={20} color={colors.green[200]} />
+          <Input.Field placeholder='Código do ingresso' />
+        </Input>
+
+        <Button title='Acessar credencial' />
+
+        <Link href="/register" className='text-orange-100 text-base font-medium text-center mt-4'>Ainda não possui ingresso?</Link>
+      </View>
     </View>
   )
 } 
