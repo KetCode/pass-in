be20392b88d7b8266b75ff6f74ed3010f46f7e6c
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-18 00:26:07 -0300

    imp: alert, handle function and get text

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,20 @@
-import {View, Image, StatusBar} from 'react-native'
+import {View, Image, StatusBar, Alert} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Link } from 'expo-router'
 import { colors } from '@/styles/colors'
 import { Input } from '@/components/input'
 import { Button } from '@/components/button'
+import { useState } from 'react'
 
 export default function Home() {
+  const [code, setCode] = useState("")
+
+  function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert("Ingresso", "Informe o código do ingresso!")
+    }
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle='light-content' />
@@ -15,10 +24,10 @@ export default function Home() {
       <View className='w-full mt-12 gap-3'>
         <Input>
           <MaterialCommunityIcons name='ticket-confirmation-outline' size={20} color={colors.green[200]} />
-          <Input.Field placeholder='Código do ingresso' />
+          <Input.Field placeholder='Código do ingresso' onChangeText={setCode} />
         </Input>
 
-        <Button title='Acessar credencial' />
+        <Button title='Acessar credencial' onPress={handleAccessCredential} />
 
         <Link href="/register" className='text-orange-100 text-base font-medium text-center mt-4'>Ainda não possui ingresso?</Link>
       </View>
