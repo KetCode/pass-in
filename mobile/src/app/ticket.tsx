fb05669c4f44314415f72eba3323c5cf85dad6bb
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-19 12:44:47 -0300

    feat: ticket page

---
+++ mobile/src/app/ticket.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Alert, ScrollView, StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'
+import * as ImagePicker from 'expo-image-picker'
+
+import { colors } from "@/styles/colors";
+import { Header } from "@/components/header";
+import { Button } from "@/components/button";
+import { Credential } from "@/components/credential";
+
+export default function Ticket() {
+  const [image, setImage] = useState('')
+
+  async function handleSelectImage() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        allowsEditing: true,
+        aspect: [4, 4],
+      })
+
+      if (result.assets){
+        setImage(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Foto", "Não foi possível selecionar a imagem.")
+    }
+  }
+
+  return (
+    <View className="flex-1 bg-green-500">
+      <StatusBar barStyle='light-content' />
+      <Header title="Minha Credencial" className="z-10" />
+
+      <ScrollView className="-mt-28" contentContainerClassName="px-8 pb-8" showsVerticalScrollIndicator={false}>
+        <Credential image={image} onChangeAvatar={handleSelectImage} />
+
+        <FontAwesome name="angle-double-down" size={24} color={colors.gray[300]} className="self-center my-6" />
+        <Text className="text-white font-bold text-2xl mt-4">Compartilhar credencial</Text>
+        <Text className="text-white font-regular text-base mt-1 mb-6">Mostre ao mundo que você vai participar do Unite Summit!</Text>
+
+        <Button title="Compartilhar" />
+        <TouchableOpacity activeOpacity={0.7} className="mt-10">
+          <Text className="text-white font-bold text-base text-center">Remover ingresso</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  )
+}
