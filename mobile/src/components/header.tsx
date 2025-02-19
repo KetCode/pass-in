b1efed8fa33dade26d6e79cabcc067e6d3ac64a1
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-19 13:18:25 -0300

    rem: view props and add z index

--- mobile/src/components/header.tsx
+++ mobile/src/components/header.tsx
@@ -1,12 +1,12 @@
-import { View, Text, ViewProps } from "react-native";
+import { View, Text } from "react-native";
 
-type Props = ViewProps & {
+type Props = {
   title: string
 }
 
 export function Header({ title }: Props) {
   return (
-    <View className="w-full h-28 flex-row items-end bg-black/20 px-8 pb-4 border-b border-white/10">
+    <View className="w-full h-28 flex-row items-end bg-black/20 px-8 pb-4 border-b border-white/10 z-10">
       <Text className="flex-1 text-white font-medium text-lg text-center">{title}</Text>
     </View>
   )
