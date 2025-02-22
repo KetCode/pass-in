c55c26c524de793727df41f880c43c363458f392
KesseT <64742407+KetCode@users.noreply.github.com>
2025-02-22 11:19:52 -0300

    feat: credential animated

--- mobile/src/components/credential.tsx
+++ mobile/src/components/credential.tsx
@@ -1,5 +1,7 @@
-import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Image, ImageBackground, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
+import { MotiView } from 'moti'
+
 import { colors } from "@/styles/colors";
 import { QRCode } from "@/components/qrcode";
 import { BadgeStore } from "@/store/badge-store";
@@ -12,8 +14,15 @@ type Props = {
 }
 
 export function Credential({ data, onChangeAvatar, onExpandQRCode }: Props) {
+  const { height } = useWindowDimensions()
+
   return (
-    <View className="w-full self-stretch items-center">
+    <MotiView 
+      className="w-full self-stretch items-center"
+      from={{ opacity: 0, translateY: -height, rotateZ: "50deg", rotateY: "30deg", rotateX: "30deg" }}
+      animate={{ opacity: 1, translateY: 0, rotateZ: "0deg", rotateY: "0deg", rotateX: "0deg"  }}
+      transition={{ type: "spring", damping: 20, rotateZ: { damping: 15, mass: 3 } }}
+    >
       <Image source={require('@/assets/ticket/band.png')} className="w-24 h-52 z-10" />
       
       <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 mx-3 rounded-2xl -mt-5">
@@ -46,6 +55,6 @@ export function Credential({ data, onChangeAvatar, onExpandQRCode }: Props) {
           <Text className="font-body text-sm text-orange-500">Ampliar QRCode</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   )
 }
